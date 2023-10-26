094aa9e2cfe50b14d122120984a75643fda98a89
MoeX <54111871+MengXin001@users.noreply.github.com>
2023-10-26 13:14:11 +0800

    prospect: MWRI-RM
    A prospective version for MWRI-RM

--- FY3D_MWRI.py
+++ FY3D_MWRI.py
@@ -4,21 +4,31 @@ import h5py
 import numpy as np
 import pandas as pd
 
-def convert(band):
-    band = band * 0.01 + 273.15
+def convert(band, intercept):
+    band = band * 0.01 + intercept
     return band
     
 def openfile(fname):
     file = h5py.File(fname, "r")
-    lons, lats = file["Geolocation"]["Longitude"][:], file["Geolocation"]["Latitude"][:]
-    btds = file["Calibration"]["EARTH_OBSERVE_BT_10_to_89GHz"]
+    if file.attrs["Satellite Name"] == "FY-3G":
+        lons, lats = file["S1"]["Geolocation"]["Longitude"][:], file["S1"]["Geolocation"]["Latitude"][:]
+        btds = file["S1"]["Data"]["EARTH_OBSERVE_BT_10_to_89GHz"]
+    else:
+        lons, lats = file["Geolocation"]["Longitude"][:], file["Geolocation"]["Latitude"][:]
+        btds = file["Calibration"]["EARTH_OBSERVE_BT_10_to_89GHz"]
     btlist = ["bt_10v","bt_10h","bt_18v","bt_18h","bt_23v","bt_23h","bt_36v","bt_36h","bt_89v","bt_89h"]
     bandnum = 0
+    intercept = btds.attrs["Intercept"]
     for var in btlist:
-        globals()[var] = btds[bandnum]
-        bandnum += 1
+        if file.attrs["Satellite Name"] == "FY-3G":
+            globals()[var] = btds[:,:,bandnum]
+            bandnum += 1
+        else: 
+            globals()[var] = btds[bandnum]
+            bandnum += 1
     for var in btlist:
-        globals()[var] = convert(eval(var))
+        globals()[var] = convert(eval(var), intercept)
+
     return bt_10v, bt_10h, bt_18v, bt_18h, bt_23v, bt_23h, bt_36v, bt_36h, bt_89v, bt_89h, lons, lats
 
 
@@ -47,4 +57,3 @@ ax.set_extent([111, 117, 20, 26],crs=crs)
 plt.pcolormesh(lons, lats, bt_89v, transform=crs)
 cbar = plt.colorbar()
 cbar.set_label(r'Brightness Temperature (K)')
-plt.savefig("pyplot_crs.png", dpi=500, bbox_inches='tight')
